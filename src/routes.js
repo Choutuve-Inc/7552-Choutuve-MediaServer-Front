f273d3f4e0be17176aefd723a40db9bd547f9000
desuque <ldesuque@fi.uba.ar>
2020-05-30 12:34:00 +0200

    first version MediaServer front :)

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+import Dashboard from "views/Dashboard.jsx";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-chart-pie-36",
+    component: Dashboard,
+    layout: "/admin"
+  },
+];
+export default routes;
---
+++ src/views/Dashboard.jsx
@@ -0,0 +1,195 @@
+import React from "react";
+// react plugin used to create charts
+import { Line, Pie } from "react-chartjs-2";
+// reactstrap components
+import {
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  CardTitle,
+  Row,
+  Col
+} from "reactstrap";
+// core components
+import {
+  dashboard24HoursPerformanceChart,
+  dashboardEmailStatisticsChart,
+  dashboardNASDAQChart
+} from "variables/charts.jsx";
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="content">
+          <Row>
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-spaceship text-warning" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Total videos</p>
+                        <CardTitle tag="p">1452</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Today
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-spaceship text-warning" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Total videos</p>
+                        <CardTitle tag="p">1452</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Today
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-spaceship text-warning" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Total videos</p>
+                        <CardTitle tag="p">1452</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Today
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+
+            <Col lg="3" md="6" sm="6">
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4" xs="5">
+                      <div className="icon-big text-center icon-warning">
+                        <i className="nc-icon nc-spaceship text-warning" />
+                      </div>
+                    </Col>
+                    <Col md="8" xs="7">
+                      <div className="numbers">
+                        <p className="card-category">Total videos</p>
+                        <CardTitle tag="p">1452</CardTitle>
+                        <p />
+                      </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                  <hr />
+                  <div className="stats">
+                    <i className="fas fa-sync-alt" /> Today
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+          <Row>
+            <Col md="4">
+              <Card>
+                <CardHeader>
+                  <CardTitle tag="h5">Users Statistics</CardTitle>
+                  <p className="card-category">User DB</p>
+                </CardHeader>
+                <CardBody>
+                  <Pie
+                    data={dashboardEmailStatisticsChart.data}
+                    options={dashboardEmailStatisticsChart.options}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="legend">
+                    <i className="fa fa-circle text-primary" /> Activos{" "}
+                    <i className="fa fa-circle text-warning" /> Sin videos{" "}
+                    <i className="fa fa-circle text-danger" /> Eliminados{" "}
+                    {/* <i className="fa fa-circle text-gray" /> Unopened */}
+                  </div>
+                  <hr />
+                  <div className="stats">
+                    <i className="fa fa-calendar" /> Number of users
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+            <Col md="8">
+              <Card className="card-chart">
+                <CardHeader>
+                  <CardTitle tag="h5">Videos/Comentarios por dia</CardTitle>
+                  <p className="card-category">Videos/comentarios cargados a la aplicacion</p>
+                </CardHeader>
+                <CardBody>
+                  <Line
+                    data={dashboardNASDAQChart.data}
+                    options={dashboardNASDAQChart.options}
+                    width={400}
+                    height={100}
+                  />
+                </CardBody>
+                <CardFooter>
+                  <div className="chart-legend">
+                    <i className="fa fa-circle text-info" /> Videos{" "}
+                    <i className="fa fa-circle text-warning" /> Comentarios
+                  </div>
+                  <hr />
+                  <div className="card-stats">
+                    <i className="fa fa-check" /> Choutuve info
+                  </div>
+                </CardFooter>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Dashboard;
