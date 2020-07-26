31e2c682a3495ba917a760e52b6eaf79719d536b
Santiago39353535 <31159922+Santiago39353535@users.noreply.github.com>
2020-07-26 20:14:09 -0300

    administracion de usuarios

--- src/views/Users.jsx
+++ src/views/Users.jsx
@@ -20,9 +20,8 @@ import {
 
 import { Route, Redirect } from "react-router";
 
-const API = 'https://arcane-thicket-79100.herokuapp.com/';
-const DEFAULT_QUERY = 'videos';
-const DELETE_QUERY = 'videos';
+const API = 'https://serene-shelf-10674.herokuapp.com';
+const DEFAULT_QUERY = '/users';
 
 class Users extends React.Component {
 
@@ -41,21 +40,38 @@ class Users extends React.Component {
       headers.append('Content-Type', 'application/json');
       headers.append('Accept', 'application/json');
       headers.append('token',localStorage.getItem('token'));
+      fetch(API + DEFAULT_QUERY, {
+        mode: 'cors',
+        method: 'GET',
+        headers: headers,
+      }).then((response) => {
+        if (response.status == 200) {
+        }
+        return response.json();
+        
+      }).then((result) => {
+        this.setState({
+          isLoaded: true,
+          items: result
+        });
+        console.log("los Usuarios son: ",result)
+      });
     }
   
     handleClick = itemId => {
-      const requestOptions = {
+      console.log("se borra el item: ", itemId)
+      /*const requestOptions = {
         method: 'DELETE'
       };
   
-      fetch(API + DELETE_QUERY + "/" + itemId, requestOptions).then((response) => {
+      fetch(API + DEFAULT_QUERY + "/" + itemId, requestOptions).then((response) => {
         if (response.status == 200) {
           this.componentDidMount()
         }
         return response;
         
       }).then((result) => {
-      });
+      });*/
     }
   
     render() {
@@ -64,14 +80,37 @@ class Users extends React.Component {
           <Redirect to="login"></Redirect>
         );
       }
+      const { items } = this.state;
       return (
         <>
           <div className="content">
-            <Row>  
-                <Card>
-                    <CardTitle>Hola</CardTitle>
-                </Card>
-            </Row>
+          {items.map(item =>
+            <Col lg="12" md="12" sm="12" key={item.uid}>
+              <Card className="card-stats">
+                <CardBody>
+                  <Row>
+                    <Col md="4">
+                        <div className="numbers">
+                          <CardTitle tag="p">{item.displayName}</CardTitle>
+                          <p className="card-category">Mail: {item.email}</p>
+                          <p className="card-category">Phone number: {item.phoneNumber}</p>
+                        </div>
+                    </Col>
+                  </Row>
+                </CardBody>
+                <CardFooter>
+                    <hr />
+                    <div className="stats">
+                      {console.log('id a borrar', item.id)}
+                      <button className="btn btn-danger" onClick={() => { this.handleClick(item.uid) }}>
+                        <i className="fas fa-trash"></i>
+                        Delete
+                      </button>
+                    </div>
+                  </CardFooter>
+              </Card>
+            </Col>
+          )}
           </div>
         </>
       );
